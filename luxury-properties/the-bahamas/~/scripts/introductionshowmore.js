abc879c25833d249908d8ab9376a86c0f895f76b
p3xcnrtm <essyenpeter57@gmail.com>
2025-11-24 22:17:21 +0100

    Add files via upload

---
+++ luxury-properties/the-bahamas/~/scripts/introductionshowmore.js
@@ -0,0 +1,18 @@
+﻿
+$(document).ready(function () {
+
+    //This linked to from introduction.ascx
+    //The reason it is in a separate file is that using defer on jquery load breaks the control if it is in the usercontrol or parent page.
+
+    $(".readMore").hide();
+    $("#divShowMore").on("click", function () {
+
+        var txt = $(".readMore").is(':visible') ? 'Read More' : 'Read Less';
+        $("#divShowMore").text(txt);
+        $('.readMore').slideToggle(800);
+    });
+
+
+});
+
+
